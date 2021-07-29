7baca0eebb353a33620723155e25ee643d8c9a26
Myriam <wicnmys@gmail.com>
2021-07-29 12:38:35 +0300

    load test data

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ import numpy as np
 # 1. Load, normalize and concatenate the first 4 days as training data , sliding window k=360.
 folder_path = "/home/myriam/projects/emporus-ml-hw/data"
 training_data_filenames = ["2018-01-02.npy", "2018-01-03.npy", "2018-01-04.npy", "2018-01-05.npy"]
-test_data_source = ["2018-01-08.npy"]
+test_data_source = "2018-01-08.npy"
 
 
 first = True
@@ -31,4 +31,9 @@ for filename in training_data_filenames:
         except ValueError:
             "data error: dimension mismatch along dates."
 
+#2. Load and normalize the last day as test data, sliding window k=360.
+file_path = os.sep.join([folder_path, test_data_source])
+data = dpr.load_data(file_path)
+test_data = np.expand_dims(data, axis=0)
 
+print(test_data.shape)
